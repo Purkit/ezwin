7b64f361626907ae69eca11e54608873f46a546b
Mriganka Purkit <corpmrv45@gmail.com>
2025-04-30 19:35:01 +0530

    see diff

--- include/ezwin/ezwin.h
+++ include/ezwin/ezwin.h
@@ -1,21 +1,21 @@
 #ifndef EZWIN_API_H
 #define EZWIN_API_H
 
-#include <stdint.h>
 #include "config.h"
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #if defined(EZWIN_IS_BUILT_AS_SHARED_LIB)
-    #if defined(_WIN32)
-        #define EZAPI __declspec(dllimport)
-    #elif defined(__GNUC__)
-        #define EZAPI
-    #endif
+#if defined(_WIN32)
+#define EZAPI __declspec(dllimport)
+#elif defined(__GNUC__)
+#define EZAPI
+#endif
 #elif defined(EZWIN_IS_BUILT_AS_STATIC_LIB)
-    #define EZAPI
+#define EZAPI
 #endif
 
 //// * EZ INPUT KEY ENUMS * ////
@@ -157,7 +157,6 @@ typedef enum _ModifierKeys {
     EZ_NUM_LOCK
 } EzModifierKeys;
 
-
 //// * FUNCTION POINTER TYPES FOR CALLBACK * ////
 struct EzWindow;
 typedef void (*fp_resize)(EzWindow *window);
@@ -177,7 +176,6 @@ typedef void (*fp_mouseLeave)(EzWindow *window);
 typedef void (*fp_windowCloseBtnPress)(EzWindow *window);
 typedef void (*fp_windowMaximize)(EzWindow *window);
 typedef void (*fp_windowMinimize)(EzWindow *window);
-
 
 //// * EZ WINDOW * ////
 struct EzWindow {
@@ -291,45 +289,53 @@ struct _EzEventListeners {
 typedef struct _EzEventListeners EzEventListeners;
 
 //// * EZ PUBLIC API * ////
-int EzInitializePlatform();
+EZAPI int EzInitializePlatform();
 
 // WindownManagement API:
-void CreateWindowS(EzWindow *window, int width, int height, const char *title);
-EzWindow *CreateWindowH(int width, int height, const char *title);
-void DestroyWindow(EzWindow *window);
-void PollEvents();
-bool WindowShouldClose(EzWindow *window);
-void SetWindowTitle(EzWindow *window, const char *title);
-void SetWindowSize(EzWindow *window, int width, int height);
-void GetWindowSize(EzWindow *window, int *width, int *height);
-void ToggleFullScreen(EzWindow *window);
-void ToggleBorderless(EzWindow *window);
-void ShowCursor(EzWindow *window, bool visible);
-void SetIcon(EzWindow *window, const char *iconPath);
+EZAPI void CreateWindowS(EzWindow *window, int width, int height,
+                         const char *title);
+EZAPI EzWindow *CreateWindowH(int width, int height, const char *title);
+EZAPI void DestroyWindow(EzWindow *window);
+EZAPI void PollEvents();
+EZAPI bool WindowShouldClose(EzWindow *window);
+EZAPI void SetWindowTitle(EzWindow *window, const char *title);
+EZAPI void SetWindowSize(EzWindow *window, int width, int height);
+EZAPI void GetWindowSize(EzWindow *window, int *width, int *height);
+EZAPI void ToggleFullScreen(EzWindow *window);
+EZAPI void ToggleBorderless(EzWindow *window);
+EZAPI void ShowCursor(EzWindow *window, bool visible);
+EZAPI void SetIcon(EzWindow *window, const char *iconPath);
 
 // Input Handling API:
-bool GetKeyState(EzWindow *window, EzKeyCode key);
-bool GetMouseButtonState(EzWindow *window, EzMouseButtonCode button);
-void GetMousePosition(EzWindow *window, float *x, float *y);
+EZAPI bool GetKeyState(EzWindow *window, EzKeyCode key);
+EZAPI bool GetMouseButtonState(EzWindow *window, EzMouseButtonCode button);
+EZAPI void GetMousePosition(EzWindow *window, float *x, float *y);
 
-void SetKeyEventCallback(EzWindow *window, fp_keyEvent callback);
-void SetMouseButtonEventCallback(EzWindow *window, fp_mouseBtnEvent callback);
-void SetPointerMoveEventCallback(EzWindow *window, fp_mouseMoveEvent callback);
-void SetMouseScrollEventCallback(EzWindow *window,
-                                 fp_mouseScrollEvent callback);
-void SetWindowReizeEventCallback(EzWindow *window, fp_resize callback);
-void SetWaylandRenderSignalCallback(EzWindow *window, fp_render callback);
-void SetWindowFocusLoseCallback(EzWindow *window, fp_windowFocusLoss callback);
-void SetWindowFocusGainCallback(EzWindow *window, fp_windowFocusGain callback);
-void SetMouseEnterCallback(EzWindow *window, fp_mouseEnter callback);
-void SetMouseLeaveCallback(EzWindow *window, fp_mouseLeave callback);
-void SetWindowMaximizeCallback(EzWindow *window, fp_windowMaximize callback);
-void SetWindowMinimizeCallback(EzWindow *window, fp_windowMinimize callback);
+EZAPI void SetKeyEventCallback(EzWindow *window, fp_keyEvent callback);
+EZAPI void SetMouseButtonEventCallback(EzWindow *window,
+                                       fp_mouseBtnEvent callback);
+EZAPI void SetPointerMoveEventCallback(EzWindow *window,
+                                       fp_mouseMoveEvent callback);
+EZAPI void SetMouseScrollEventCallback(EzWindow *window,
+                                       fp_mouseScrollEvent callback);
+EZAPI void SetWindowReizeEventCallback(EzWindow *window, fp_resize callback);
+EZAPI void SetWaylandRenderSignalCallback(EzWindow *window, fp_render callback);
+EZAPI void SetWindowFocusLoseCallback(EzWindow *window,
+                                      fp_windowFocusLoss callback);
+EZAPI void SetWindowFocusGainCallback(EzWindow *window,
+                                      fp_windowFocusGain callback);
+EZAPI void SetMouseEnterCallback(EzWindow *window, fp_mouseEnter callback);
+EZAPI void SetMouseLeaveCallback(EzWindow *window, fp_mouseLeave callback);
+EZAPI void SetWindowMaximizeCallback(EzWindow *window,
+                                     fp_windowMaximize callback);
+EZAPI void SetWindowMinimizeCallback(EzWindow *window,
+                                     fp_windowMinimize callback);
 
-void RegisterEventListeners(EzWindow *window, const EzEventListeners *listeners);
+EZAPI void RegisterEventListeners(EzWindow *window,
+                                  const EzEventListeners *listeners);
 
-void EzFreeInternalPlatformState(EzWindow *window);
-void EzFreeWindow(EzWindow *window);
+EZAPI void EzFreeInternalPlatformState(EzWindow *window);
+EZAPI void EzFreeWindow(EzWindow *window);
 
 // clang-format off
 #define EzCreateWindow(arg, ...)          \
